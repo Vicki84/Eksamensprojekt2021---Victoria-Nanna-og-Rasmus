19a326f46ce1fccf9108af7226c1098716663830
jiuras <57357540+jiuras@users.noreply.github.com>
2021-05-05 10:41:07 +0200

    Tilføjelse af regnefunktion, andre valuter samt nyt layout

--- valutaomregner.js
+++ valutaomregner.js
@@ -1,11 +1,35 @@
 const api_url = 'http://api.exchangeratesapi.io/v1/latest?access_key=808c330fd03e791ededfecee53e2e3e3'
-async function rasmus() {
+async function rasmus(form) {
 const response = await fetch(api_url)
+const data = await response.json()
+let valuta1 = form.list1.selectedIndex, valuta2 = form.list2.selectedIndex, valuta1Name, valuta2Name, valutaValue = form.inputbox1.value, valutaResult
 
-const data = await response.json();
-let {DKK, EUR} = data.rates
-let currency = 'DKK= ' + DKK + " EUR= " + EUR
-console.log(currency)
+switch (valuta1) {
+    case 0:
+        valuta1Name = data.rates.DKK;
+        break;
+
+    case 1:
+        valuta1Name = data.rates.EUR;
+        break;
+    case 2:
+        valuta1Name = data.rates.USD;
+        break;
 }
 
-rasmus()
+switch (valuta2) {
+    case 0:
+        valuta2Name = data.rates.DKK;
+        break;
+    case 1:
+        valuta2Name = data.rates.EUR;
+        break;
+    case 2:
+        valuta2Name = data.rates.USD;
+        break;
+}
+
+valutaResult = valutaValue / valuta1Name * valuta2Name;
+valutaResult = valutaResult.toString().substring(0,5);
+    form.inputbox2.value = valutaResult;
+}
